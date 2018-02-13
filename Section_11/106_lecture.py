78820f7a2f785c2839dcc1945d83ee9037458323
Maciej Wasilewski <maciej0wasilewski@gmail.com>
2018-02-13 13:12:40 +0100

    Finished Lectures 105-106 and started lecture 107

---
+++ Section_11/106_lecture.py
@@ -0,0 +1,9 @@
+import blackjack
+
+# blackjack._deal_card(blackjack.dealer_card_frame)
+# print(__name__)
+# blackjack.play()
+
+personal_details = ("Maciek", 26, "Polska")
+name, _, country = personal_details
+print(name, country)
